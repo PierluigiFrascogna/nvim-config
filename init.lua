833ef8149a0322e62403dccf3db5532238ddd31a
Pierluigi Frascogna <frascogna.pierluigi@gmail.com>
2026-05-31 20:24:57 +0200

    added insert mode keymaps

--- init.lua
+++ init.lua
@@ -154,3 +154,6 @@ vim.keymap.set('n', '<leader>ot', vimcmd ':terminal', { desc = '[O]pen [T]ermina
 
 vim.pack.add({ { src = gh 'saghen/blink.cmp', version = vim.version.range('1.*'), }, })
 require('blink.cmp').setup()
+--
+vim.keymap.set('i', '<S-Backspace>', '<C-c>cb<del>',{desc='long backspace'})
+vim.keymap.set('i', '<S-Enter>', '<C-X><C-O>', {desc='accept autocompletion'})
